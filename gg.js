dfbd82bb3427f3aca55244fbca51f81cab6f515d
ehmmedova25 <guldastama-af107@code.edu.az>
2025-03-26 21:41:42 +0400

    Add files via upload

---
+++ gg.js
@@ -0,0 +1,127 @@
+/**1-dən 100-ə qədər olan ədədlərin cəmini tapın. */
+/*let cem = 0;
+
+for (let i = 1; i <= 100; i++) {
+  cem += i;
+}
+
+console.log( cem);*/
+
+
+
+
+
+
+/*Verilmiş array: [1, 2, 3, 4, 5] – bu array-dəki bütün ədədləri konsola yazdırın.for in*/
+/*const numbers = [1, 2, 3, 4, 5];
+
+for (const index in numbers) {
+  console.log(numbers[index]);
+}*/
+
+
+
+
+/*Verilmiş obyekt: {ad: 'Ali', yaş: 25, şəhər: 'Bakı'} – bu obyektin bütün açarlarını konsola yazdırın.*/
+
+/*const kisi = {
+    ad: 'Ali',
+    yaş: 25,
+    şəhər: 'Bakı'
+  };
+  
+  const keys = Object.keys(kisi);
+  console.log(keys);*/
+
+
+
+
+
+
+
+/**· Verilmiş obyekt: {kitab: '1984', müəllif: 'George Orwell'} – açar və dəyərləri konsola yazdırın.for of */
+/*const siyahi = {kitab: '1984', müəllif: 'George Orwell'};
+
+for (const [key, value] of Object.entries(siyahi)) {
+  console.log(key + ': ' + value);
+}*/
+
+
+
+
+
+
+
+/**Verilmiş array: ['alma', 'armud', 'banan'] – bütün meyvələri konsola yazdırın.*/
+/*let meyveler = ['alma', 'armud', 'banan'];
+let i = 0;
+
+do {
+  console.log(meyveler[i]);
+  i++;
+} while (i < meyveler.length);*/
+
+
+
+
+
+/**Verilmiş string: 'Salam' – hər bir hərfi konsola yazdırın.while */
+/**let soz = 'Salam';
+let i = 0;
+
+while (i < soz.length) {
+  console.log(soz[i]);
+  i++;
+} */
+
+
+
+
+
+/*1-dən 10-a qədər olan ədədləri konsola yazdırın. */
+/*let i = 1;
+do {
+  console.log(i);
+  i++;
+} while (i <= 10);*/
+
+
+
+
+
+
+/*Verilmiş array: [5, 10, 15, 20] – array boşalana qədər elementləri konsola yazdırın.do while*/
+/*let ededler= [5, 10, 15, 20];
+let i = 0;
+
+do {
+  console.log(ededler[i]);
+  i++;
+} while (i < ededler.length);*/
+
+
+
+
+
+/*1-dən 5-ə qədər ədədləri do while ilə konsola yazdırın.*/
+/*let i = 0;
+do {
+  i++;
+  console.log(i);
+} while (i < 5);*/
+
+
+
+
+
+
+
+
+/*Verilmiş şərt: istifadəçi ədəd daxil edir və ədəd 50-dən kiçikdirsə döngü davam edir.*/
+/*let eded;
+
+do {
+  eded = prompt("ededi  daxil edin");
+} while (eded < 50);
+
+console.log("Döngü bitdi daxil edilen eded 50 den boyukdur");*/
